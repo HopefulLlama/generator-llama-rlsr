8790628404d04b8c58ddf4dc8318dc3ac84fb619
Jonathan Law <jonlaw10170@gmail.com>
2017-09-01 01:42:22 +0100

    Prep things for release maybe

--- llama-rlsr.config.js
+++ llama-rlsr.config.js
@@ -1,18 +1,51 @@
 const LlamaRlsrNpm = require('llama-rlsr-npm');
 const LlamaRlsrKeepAChangelog = require('llama-rlsr-keep-a-changelog');
+const simpleGit = require('simple-git')(process.cwd());
+const GitHubApi = require('github-api');
 
-module.exports = [
-  LlamaRlsrNpm.updateVersion(),
-  LlamaRlsrKeepAChangelog.updateChangelog({
-    placeholder: '- Nothing yet'
-  }),
-  LlamaRlsrKeepAChangelog.updateDiff({
-    urlGenerator: (oldVersion, newVersion) => {
-      return `https://github.com/HopefulLlama/generator-llama-rlsr/compare/${oldVersion}...${newVersion}`;
+const gitHubCredentials = require('./credentials/github.json');
+
+if(!gitHubCredentials) {
+  throw new Error('GitHub credentials not found.');
+}
+
+module.exports = {
+  preRelease: [
+    LlamaRlsrNpm.updateVersion(),
+    LlamaRlsrKeepAChangelog.updateChangelog({
+      placeholder: '- Nothing yet'
+    }),
+    LlamaRlsrKeepAChangelog.updateDiff({
+      urlGenerator: (oldVersion, newVersion) => {
+        return `https://github.com/HopefulLlama/generator-llama-rlsr/compare/${oldVersion}...${newVersion}`;
+      },
+      latest: 'HEAD',
+      tag: {
+        prefix: 'v'
+      }
+    })
+  ],
+  release: [
+    (versionMetadata, done) => {
+      simpleGit.add(['package.json', 'CHANGELOG.md', 'llama-rlsr.metadata.json'], () => {
+        simpleGit.commit(`Update to version ${versionMetadata.newVersion}`, () => {
+          simpleGit.addTag(`v${versionMetadata.newVersion}`, () => {
+            simpleGit.push('origin', 'master', () => {
+              simpleGit.pushTags('origin', () => {
+                done();
+              });
+            });
+          });
+        });
+      });
     },
-    latest: 'HEAD',
-    tag: {
-      prefix: 'v'
+    (versionMetadata, done) => {
+      let gitHub = new GitHubApi(gitHubCredentials);
+      gitHub
+      .getRepo('HopefulLlama', 'generator-llama-rlsr')
+      .createRelease({"tag_name": `v${versionMetadata.newVersion}`}, () => {
+        done();
+      });
     }
-  })
-];
+  ]
+};
